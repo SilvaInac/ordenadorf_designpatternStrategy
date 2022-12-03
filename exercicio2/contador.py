58b01bf33e5cab1e41f1004ed5ea71fe90d94c09
SilvaInac <eduardo040silva@gmail.com>
2022-12-03 02:15:59 -0300

    add contadores and gitignore

--- exercicio2/contador.py
+++ exercicio2/contador.py
@@ -8,9 +8,8 @@ class ContadorPalavras(Observador):
 
     def update(self):
         res = len(self.frase.split())
-        print ("The number of words in string are : " +  str(res)) 
+        print ("Numero total de palavras : " +  str(res)) 
         return res
-
 
 class ContadorPalavrasCaracteresPares(Observador):
     
@@ -18,6 +17,23 @@ class ContadorPalavrasCaracteresPares(Observador):
         self.frase = frase
 
     def update(self):
-        res = len(self.frase.split())
-        print ("The number of words in string are : " +  str(res)) 
-        return res
+        count = 0
+        listaPalavras = self.frase.split()
+        for palavra in listaPalavras:
+            if(len(palavra)%2 ==0):
+                count = count+1
+        print ("Numero de palavras que começam com letra maiuscula: " + str(count)) 
+        return count
+
+class ContadorPalavrasComecamMaiuscula(Observador):
+    def __init__(self, frase : str):
+        self.frase = frase
+
+    def update(self):
+        count = 0
+        listaPalavras = self.frase.split()
+        for palavra in listaPalavras:
+            if(palavra[0].isupper() ==True):
+                count = count+1
+        print ("Numero de palavras com quantidades pares de caracteres: " + str(count)) 
+        return count
--- exercicio2/main.py
+++ exercicio2/main.py
@@ -1,10 +1,14 @@
-from contador import ContadorPalavras
+from contador import ContadorPalavras, ContadorPalavrasCaracteresPares, ContadorPalavrasComecamMaiuscula
 from frase import Orquestrador
 
-fraseAleatoria = 'Ola tudo bem com voce'
+fraseAleatoria = 'Ola tudo bem com voce Garoto'
 
 
 manipulador1 = Orquestrador()
 obs1 = ContadorPalavras(frase=fraseAleatoria)
+obs2 = ContadorPalavrasCaracteresPares(fraseAleatoria)
+obs3 = ContadorPalavrasComecamMaiuscula(fraseAleatoria)
 manipulador1.addFunc(obs1)
+manipulador1.addFunc(obs2)
+manipulador1.addFunc(obs3)
 manipulador1.order()
