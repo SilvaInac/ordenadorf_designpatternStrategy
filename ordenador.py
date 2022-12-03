136da95578b379ce80383fe1d3b5d4daf867126e
SilvaInac <eduardo040silva@gmail.com>
2022-12-03 00:06:09 -0300

    add ordenador

--- ordenador.py
+++ ordenador.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractclassmethod
+import random
 
 #Strategy interface
 class Strategy(ABC):
@@ -8,18 +9,63 @@ class Strategy(ABC):
     def selection(self) -> None:
         pass
     
-class crescente(Strategy):
-    
-    def __init__(self,listaData) -> None:
-        self.listaData = []
-        self.listaData = listaData
-        
-    def selection(self) -> str:
-        return self.listaData.sort()
+class Crescente(Strategy):
+    def selection(self,lista) -> str:
+        self.newList  = []
+        for i in lista:
+            self.newList.append(i)
 
-class Ordenar:
+        self.newList.sort()
+        return (self.newList)
+
+class Decrescente(Strategy):
+    def selection(self,lista) -> str:
+        self.newList  = []
+        for i in lista:
+            self.newList.append(i)
+
+        self.newList.sort(reverse=1)
+        return (self.newList)
+
+
+class Random(Strategy):
+    def __init__(self,lista: list) -> None:
+        if lista is not None:
+                self.listData = lista
+
+    def selection(self) -> str:
+        options = [Crescente, Decrescente]
+        algoritmo =  random.choice(options)
+
+        if(algoritmo == Crescente):
+            return self.listData.sort()
+
+
+class Ordenador:
+    strategy: Strategy
     """
     The Context defines the interface of interest to clients.
     """
-    def __init__(self,strategy: Strategy) -> None:
-        self._strategy = strategy
+    def __init__(self,strategy: Strategy = None) -> None:
+        if strategy is not None:
+                self.strategy = strategy
+        else:
+            self.strategy = Random()
+    
+    def ordenar(self,lista) -> None:
+        listaOrdenada = self.strategy.selection(self,lista)
+        return listaOrdenada
+
+
+listaDesordenada = [5,1,3,2,4,7,8,9,6]
+
+listaOrdenadaA = Ordenador(Crescente)
+listaOrdenadaB = Ordenador(Decrescente)
+listaOrdenadaC = Ordenador(Random)
+
+listaA = listaOrdenadaA.ordenar(listaDesordenada)
+print("Crescente:",listaA)
+listaB = listaOrdenadaB.ordenar(listaDesordenada)
+print("Decrescente:",listaB)
+listaC = listaOrdenadaB.ordenar(listaDesordenada)
+print("Ordem Randomica:",listaC)
