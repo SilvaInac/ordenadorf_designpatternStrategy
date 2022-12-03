1bc60fa1b18891223f1d909a20228589f7f6e34b
SilvaInac <eduardo040silva@gmail.com>
2022-12-03 02:18:59 -0300

    resolve

---
+++ TestOrdenador.py
@@ -0,0 +1,37 @@
+import unittest
+from ordenador import Ordenador,Crescente,Decrescente,Random
+
+class Testing(unittest.TestCase):
+    def test_crescente(self):
+
+        listaOrdenadaA = Ordenador(Crescente)
+        listaA = listaOrdenadaA.ordenar([2,3,5,1,4,7,6])
+
+        self.assertEqual(listaA, [1,2,3,4,5,6,7])
+
+    def test_decrescente(self):
+        listaOrdenadaA = Ordenador(Decrescente)
+        listaA = listaOrdenadaA.ordenar([2,3,5,1,4,7,6])
+
+        self.assertEqual(listaA, [7,6,5,4,3,2,1])
+
+    def test_decrescente(self):
+        listaOrdenadaA = Ordenador(Decrescente)
+        listaA = listaOrdenadaA.ordenar([2,3,5,1,4,7,6])
+
+        self.assertEqual(listaA, [7,6,5,4,3,2,1])
+
+    def test_None(self):
+        listaOrdenadaA = Ordenador(Crescente)
+        listaA = listaOrdenadaA.ordenar([])
+
+        self.assertIsNotNone(listaA)
+
+    def test_Random(self):
+        listaOrdenadaA = Ordenador(Random)
+        listaA = listaOrdenadaA.ordenar([7,6,5,4,3,2,1])
+
+        self.assertEqual(listaA,[7,6,5,4,3,2,1]or[1,2,3,4,5,6,7])
+
+if __name__ == '__main__':
+    unittest.main()
